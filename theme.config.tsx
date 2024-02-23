c74c779c3048d38f991cf70fd2adad34f7bfb601
Luca <lucapribilovic466@gmail.com>
2024-02-23 23:03:48 +1000

    .

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,42 +3,30 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: (
     <>
-      <img src="/static/logo.png" style={{width: 40, height: 'auto'}} alt="Hyperscripts Logo"></img>
+      <img src="/static/logo.png" style={{width: 40, height: 'auto'}} alt="Bandit Gaming Logo"></img>
       <span style={{ marginLeft: '.4em', fontWeight: 800 }}>
-        HyperScripts Docs
+        Bandit Gaming Docs
       </span>
     </>
   ),
   head:(
       <>
-        <title>HyperScripts Docs</title>
-        <meta property="og:title" content="HyperScripts Docs" />
-        <meta property="og:description" content="HyperScripts, Elevate Your Code, Accelerate Your Success!" />
-        <meta property="og:url" content={'https://docs.pribilovic.net'} />
+        <title>Bandit Gaming Docs</title>
+        <meta property="og:title" content="Bandit Gaming Docs" />
+        <meta property="og:description" content="Bandit Gaming, Elevate Your Code, Accelerate Your Success!" />
+        <meta property="og:url" content={'https://docs.banditgaming.com.au'} />
         <meta property="og:image" content="/static/logo.png" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@HyperScripts" />
-        <meta name="twitter:title" content={'HyperScripts Docs'} />
+        <meta name="twitter:title" content={'Bandit Gaming Docs'} />
         <meta
           name="twitter:description"
-          content={'HyperScripts, Elevate Your Code, Accelerate Your Success!'}
+          content={'Bandit Gaming, Elevate Your Code, Accelerate Your Success!'}
         />
         <meta name="theme-color" content="#1a202c" />
 
-        <link rel="shortcut icon" href="https://cdn.roate.net/NoNU6/vAXaviLu54.png/raw" />
 
       </>
   ),
-  project: {
-    link: 'https://github.com/HyperScripts-FiveM',
-  },
-  chat: {
-    link: 'https://pribiovic.net/discord',
-  },
-  docsRepositoryBase: 'https://github.com/HyperScripts-FiveM/HyperScripts-Docs`',
-  footer: {
-    text: 'HyperScripts',
-  },
   sidebar: {
     defaultMenuCollapseLevel: 1,
   },
