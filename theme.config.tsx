85408b58a868e76dceee4086234a45ac174e3e6d
Luca <lucapribilovic466@gmail.com>
2024-02-23 22:48:32 +1000

    ,

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,17 +1,46 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
-
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: (
+    <>
+      <img src="/static/logo.png" style={{width: 40, height: 'auto'}} alt="Hyperscripts Logo"></img>
+      <span style={{ marginLeft: '.4em', fontWeight: 800 }}>
+        HyperScripts Docs
+      </span>
+    </>
+  ),
+  head:(
+      <>
+        <title>HyperScripts Docs</title>
+        <meta property="og:title" content="HyperScripts Docs" />
+        <meta property="og:description" content="HyperScripts, Elevate Your Code, Accelerate Your Success!" />
+        <meta property="og:url" content={'https://docs.pribilovic.net'} />
+        <meta property="og:image" content="/static/logo.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@HyperScripts" />
+        <meta name="twitter:title" content={'HyperScripts Docs'} />
+        <meta
+          name="twitter:description"
+          content={'HyperScripts, Elevate Your Code, Accelerate Your Success!'}
+        />
+        <meta name="theme-color" content="#1a202c" />
+
+        <link rel="shortcut icon" href="https://cdn.roate.net/NoNU6/vAXaviLu54.png/raw" />
+
+      </>
+  ),
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/HyperScripts-FiveM',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://pribiovic.net/discord',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/HyperScripts-FiveM/HyperScripts-Docs`',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'HyperScripts',
+  },
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
   },
 }
 
